bfda5a7e4be4366df042cdb80afb45288e645a16
RishabhNahar <90189129+rishabh-nahar@users.noreply.github.com>
2023-07-21 17:22:49 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'findmynotes2022@gmail.com'
-EMAIL_HOST_PASSWORD = '_radhe_radhe_1217'
+EMAIL_HOST_USER = '****@gmail.com'
+EMAIL_HOST_PASSWORD = '****'
 
 
 RAZOR_KEY_ID = "rzp_test_JsyLrvhKMViT99"
